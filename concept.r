328fecae201ea69b5556a69daf871bf1ec48f5c2
Elvis Ngwesse <elvisngwesseaws@gmail.com>
2022-12-28 14:09:08 +0000

    Updates

---
+++ concept.r
@@ -0,0 +1,13 @@
+
+
+*******************
+AWS Systems Manager
+*******************
+        - AWS Systems Manager centralizes operational data from multiple AWS services and automates tasks across your AWS resources
+        - You can create logical groups of resources such as applications, different layers of an application stack, or production
+          versus development environments
+        - With Systems Manager, you can select a resource group and view its recent API activity, resource configuration changes,
+          related notifications, operational alerts, software inventory, and patch compliance status
+        - It lets you take action on each resource group depending on your operational needs
+        - Systems Manager provides a central place to view and manage your AWS resources, so you can have complete visibility and
+          control over your operations
